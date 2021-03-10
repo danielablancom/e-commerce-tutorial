3ca1b204c4ab5360053e15287100019464a4ff54
danielablancom <blancodanielac@gmail.com>
2021-03-10 20:49:37 -0300

    navbar with styles and new url images for products, styles for Products component were added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
-import Product from './Product/Product'
+import Product from './Product/Product'; 
+import useStyles from './styles'; 
 
 const products = [
-  { id: 1, name: "Shoes", description: "Running shoes", price: '$5 ', image: 'https://assets.ajio.com/medias/sys_master/root/h0e/h57/14092954894366/-473Wx593H-460455972-black-MODEL.jpg' },
-  { id: 2, name: "Macbook", description: "Apple macbook", price: '$10', image: 'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/macbook-pro-13-og-202011?wid=600&hei=315&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1604347427000' },
+  { id: 1, name: "Shoes", description: "Music shoes", price: '$5 ', image: 'https://images-na.ssl-images-amazon.com/images/I/61RTP8ntYzL._AC_UY395_.jpg' },
+  { id: 2, name: "Macbook", description: "Apple Macbook", price: '$10', image: 'https://ae01.alicdn.com/kf/HTB1prx0NXXXXXavapXXq6xXFXXX5/BGtomato-love-music-art-printing-hot-t-shirts-for-men-brand-new-fashion-designer-personality-shirts.jpg' },
 ];
 
 const Products = () => {
+
+  const classes = useStyles(); 
+
   return (
-  <main>
+  <main className={classes.content}>
+    <div className={classes.toolbar}/>
     <Grid container justify="center" spacing={4}>
       {products.map((product) => (
         <Grid item key={product.id} xs={12} sx={6} md={4} lg={3}>
