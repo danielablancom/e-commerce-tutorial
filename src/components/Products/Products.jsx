a74cc76253001b34f50087446af85659f5e8a913
danielablancom <blancodanielac@gmail.com>
2021-03-10 14:23:09 -0300

    workspace, dependencies installed

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,17 @@
+import React from 'react'; 
+import Grid from '@material-ui/core'; 
+
+const Products = [
+    { id: 1, name: 'Shoes', description: 'Running shoes' },
+
+]
+
+const Products = () => {
+  <main>
+      <Grid container justify='center' spacing={4}>
+
+      </Grid>
+  </main>
+}
+
+export default Products
